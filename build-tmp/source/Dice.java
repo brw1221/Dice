e8d87277f85c255336bdb6a7f178e26057fad0e9
unknown <brwong13@697-334-Gimli.sf.k12.internal>
2015-09-09 11:20:47 -0700

    changed rows and columns

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,11 +1,27 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 Die one;
-void setup()
+public void setup()
 {
 	size(510,510);
 	noLoop();
 	textAlign(CENTER);
 }
-void draw()
+public void draw()
 {	
 	background(0);
 	int numRoll = 0;
@@ -23,7 +39,7 @@ void draw()
 	textSize(15);
 	 text("Your roll: " + numRoll, 255,499);
 }
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -37,7 +53,7 @@ class Die
 		num = (int)((Math.random()*6)+1);
 
 	}
-	void roll()
+	public void roll()
 	{
 		
 		if(num == 1)
@@ -87,9 +103,18 @@ class Die
 
 		}
 	}
-	void show()
+	public void show()
 	{
 		fill(255);
 		rect(myX, myY, 20, 20);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
